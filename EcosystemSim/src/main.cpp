33a4cacefe97ef79bc12d2a79061212657c2495e
DCT <dheemanchaitanyathakur@gmail.com>
2026-09-22 13:39:22 +0530

    did basic SFML and made a refrn sheet for basic stuff

--- EcosystemSim/src/main.cpp
+++ EcosystemSim/src/main.cpp
@@ -5,6 +5,17 @@
 int main(){
     std::cout << "Before Opening" << std::endl;
     sf::RenderWindow window(sf::VideoMode({800, 600}), "EcoSystemSim");
+    sf::Vertex vertex;
+    vertex.position = sf::Vector2f(10.f,50.f);
+    vertex.color = sf::Color::Red;
+    vertex.texCoords = sf::Vector2f(100.f,100.f);
+    sf::VertexArray Tri(sf::PrimitiveType::Triangles, 3);
+    Tri[0].position = sf::Vector2f(10.f, 10.f);
+    Tri[1].position = sf::Vector2f(100.f,100.f);
+    Tri[2].position = sf::Vector2f(100.f, 10.f);
+    Tri[0].color = sf::Color::Red;
+    Tri[1].color = sf::Color::Green;
+    Tri[2].color = sf::Color::Blue;
     while(window.isOpen()){
         sf::Event event;
         while(window.pollEvent(event)){
@@ -12,6 +23,7 @@ int main(){
                 window.close();
         }
         window.clear(sf::Color::Black);
+        window.draw(Tri);
         window.display();
     }
     std::cout << "After Opening " << std::endl;
